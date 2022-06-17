002bb2a93918879277a3c6645e5bffa955b0b2b2
jahan <jahangirtavassoli3@gmail.com>
2022-06-17 06:15:33 +0430

    first commit

---
+++ SQLQuery8.sql
@@ -0,0 +1,15 @@
+insert into Moshtari(Name,Family,Address,PhoneNumber,HomeNumber)
+values
+('Hadi','Hemati','Tehran',09126528796,0214425856),
+('Hadi','Emami','Tehran',09126548258,0216689856),
+('Ramin','Bagheri','Tehran',09126548796,0215589856),
+('Ramin','Asadi','Tehran',09126548796,0214289856),
+('RAmin','Abasi','Tehran',09126548796,0214189856),
+('Matin','Javadi','Tehran',09126548796,0217489856),
+('Mohsen','Lolaee','Tehran',09126548796,0214889856),
+('Matin','Saadi','Tehran',09126536996,0218589856),
+('Saba','Naderi','Tehran',0912601296,0218489856),
+('Saba','Samavat','Tehran',09126549896,0218889856),
+('Pari','Mohamadi','Tehran',09126558796,0218789856),
+('Parii','Karami','Tehran',09126548666,0218289856)
+;
